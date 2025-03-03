da382d9a8ef9cad859a8d84df00c6171d10abad1
Kishore Subramanian <kishore.subramanian@gmail.com>
2025-03-03 15:35:50 -0800

    Minor update to Limelight to update tx, ty values in the periodic method.

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
@@ -29,7 +30,17 @@ public class LimelightSubsystem extends SubsystemBase {
     
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
+        // Update values from NetworkTables
+        double currentX = tx.getDouble(0.0);
+        double currentY = ty.getDouble(0.0);
+        double currentArea = ta.getDouble(0.0);
+        double currentTarget = tv.getDouble(0.0);
+        
+        // You can also log these values to SmartDashboard for debugging
+        SmartDashboard.putNumber("Limelight X", currentX);
+        SmartDashboard.putNumber("Limelight Y", currentY);
+        SmartDashboard.putNumber("Limelight Area", currentArea);
+        SmartDashboard.putBoolean("Limelight Has Target", currentTarget > 0.5);
     }
     
     /**
